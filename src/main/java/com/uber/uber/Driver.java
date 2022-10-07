c5c7a378f7755b11eeba142d463b3587a793e025
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-10-07 11:48:37 +0100

    many to many done

--- src/main/java/com/uber/uber/Driver.java
+++ src/main/java/com/uber/uber/Driver.java
@@ -8,6 +8,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -16,12 +17,14 @@ import javax.persistence.ManyToMany;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Entity
 public class Driver {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.AUTO)
   public Integer id;
 
   @Column(nullable = false)
@@ -41,11 +44,41 @@ class DriverController {
   @Autowired
   private DriverRepo driverRepo;
 
+  @Autowired
+  private PassengerRepo passengerRepo;
+
   @GetMapping("/drivers")
   public List<Driver> getDrivers() {
     return driverRepo.findAll();
   }
 
+  @PostMapping("/drivers")
+  public Driver createDriver(@RequestBody Driver driverData) {
+    return driverRepo.save(driverData);
+  }
+
+  @PostMapping("/ride")
+  public Message createRide(@RequestBody DriverAndPassengerIds body) {
+    Driver driver = driverRepo.findById(body.driverId).get();
+    Passenger passenger = passengerRepo.findById(body.passengerId).get();
+    driver.passengers.add(passenger);
+    driverRepo.save(driver);
+    return new Message("Ride successfully created.");
+  }
+}
+
+class DriverAndPassengerIds {
+  public Integer driverId;
+  public Integer passengerId;
+}
+
+// const message = {}
+class Message {
+  public String message;
+
+  public Message(String message) {
+    this.message = message;
+  }
 }
 
 interface DriverRepo extends JpaRepository<Driver, Integer> {
--- src/main/java/com/uber/uber/Passenger.java
+++ src/main/java/com/uber/uber/Passenger.java
@@ -7,12 +7,15 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -20,7 +23,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class Passenger {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.AUTO)
   public Integer id;
 
   @Column(nullable = false)
@@ -42,6 +45,11 @@ class PassengerController {
   @GetMapping("/passengers")
   public List<Passenger> getPassengers() {
     return passengerRepo.findAll();
+  }
+
+  @PostMapping("/passengers")
+  public Passenger createPassenger(@RequestBody Passenger passengerData) {
+    return passengerRepo.save(passengerData);
   }
 
 }
